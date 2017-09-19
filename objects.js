de64844c35d5e75d1d3f8421958eefb55c52c129
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 20:53:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,13 @@
 var playlist = {
   Eagles : 'Already Gone',
-  Prince: '1999'
+  Prince: '1999',
+  Kanye: "Gold Digger"
 }
 
 function updatePlaylist(obj, artistName, songTitle) {
   return Object.assign( {}, obj, { [artistName] : songTitle } )
 }
 function removeFromPlaylist(playlist,artistName) {
-  delete playlist.[artistname]
+  delete playlist.artistname
   return playlist
 }
