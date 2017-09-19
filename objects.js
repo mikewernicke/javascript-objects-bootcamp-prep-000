e2fa944a6deeb864d37c38e012ad5d4e3501da74
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 20:51:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(obj, artistName, songTitle) {
   return Object.assign( {}, obj, { [artistName] : songTitle } )
 }
 function removeFromPlaylist(playlist,artistName) {
-  delete playlist.[artistname]
+  delete playlist.artistname;
   return playlist
 }
