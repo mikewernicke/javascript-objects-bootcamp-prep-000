9b91ece53fb143133401cc99d09692004e9cebad
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-19 20:02:37 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  Eagles : 'Already Gone'
+}
+function updatePlayList (playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName] : songTitle})
+}
+//function removeFromPlaylist (playlist,artistName) {
+//
+//}
